92a2b097dfe5e328e1c1b0379a2d71898102b8b9
Elayan <blue-violin@hotmail.fr>
2021-12-06 10:53:10 +0100

    Create solution and project

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
