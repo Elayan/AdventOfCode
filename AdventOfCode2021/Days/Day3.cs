fbac3479c1707883063850ea87cd0ce8c620de75
Elayan <blue-violin@hotmail.fr>
2021-12-07 11:14:53 +0100

    Day 3 part 2

--- AdventOfCode2021/Days/Day3.cs
+++ AdventOfCode2021/Days/Day3.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode2021.Helpers;
 
@@ -9,7 +10,7 @@ namespace AdventOfCode2021.Days
     /// </summary>
     public static class Day3
     {
-        public static void DecodeBinaryConsumption()
+        public static void DecodeBinaryPowerConsumption()
         {
             var binaries = FileReader.ReadBinariesFromFile("day3");
             if (binaries.Length == 0)
@@ -21,7 +22,21 @@ namespace AdventOfCode2021.Days
 
             var gammaDec = Convert.ToInt32(gamma, 2);
             var epsilonDec = Convert.ToInt32(epsilon, 2);
-            Console.WriteLine($"[03a] Decode binary : gamma * epsilon = {gamma} x {epsilon} = {gammaDec} x {epsilonDec} = {gammaDec * epsilonDec}");
+            Console.WriteLine($"[#03a] Decode binary power : gamma * epsilon = {gamma} x {epsilon} = {gammaDec} x {epsilonDec} = {gammaDec * epsilonDec}");
+        }
+
+        public static void DecodeBinaryLifeSupport()
+        {
+            var binaries = FileReader.ReadBinariesFromFile("day3");
+            if (binaries.Length == 0)
+                return;
+
+            var oxygen = ScrapeFilter(binaries, true, '1');
+            var co2 = ScrapeFilter(binaries, false, '0');
+
+            var oxygenDec = Convert.ToInt32(oxygen, 2);
+            var co2Dec = Convert.ToInt32(co2, 2);
+            Console.WriteLine($"[#03b] Decode binary life : oxygen * CO2 = {oxygen} x {co2} = {oxygenDec} x {co2Dec} = {oxygenDec * co2Dec}");
         }
 
         private static string ReverseBinary(string binary)
@@ -46,6 +61,47 @@ namespace AdventOfCode2021.Days
                 gamma[i] = ones > zeros ? '1' : '0';
             }
             return new string(gamma);
+        }
+
+        /// <summary>
+        /// Filter by removing all binaries that doesn't match criteria bit by bit, until there's one left.
+        /// </summary>
+        /// <param name="mostCommonMode">If TRUE, criteria is the most common bit. If FALSE, it's the less common.</param>
+        /// <param name="defaultIfEqualCount">Which bit value to use if there's equal count of the two</param>
+        private static string ScrapeFilter(string[] binaries, bool mostCommonMode, char defaultIfEqualCount)
+        {
+            var leftBinaries = new List<string>(binaries);
+            var binLength = binaries.First().Length;
+
+            for (var i = 0; i < binLength; i++)
+            {
+                var ones = leftBinaries.Where(b => b[i] == '1').ToList();
+                var zeros = leftBinaries.Where(b => b[i] == '0').ToList();
+
+                if (ones.Count == zeros.Count)
+                    leftBinaries = leftBinaries.Where(b => b[i] == defaultIfEqualCount).ToList();
+                else if (ones.Count > zeros.Count)
+                    leftBinaries = mostCommonMode ? ones : zeros;
+                else
+                    leftBinaries = mostCommonMode ? zeros : ones;
+
+                if (leftBinaries.Count == 0)
+                {
+                    Console.WriteLine("No binary left, scraping went wrong!");
+                    return null;
+                }
+
+                if (leftBinaries.Count == 1)
+                    return leftBinaries.First();
+            }
+            
+            if (leftBinaries.Count != 1)
+            {
+                Console.WriteLine("Scraping ended wrong!");
+                return null;
+            }
+
+            return leftBinaries.First();
         }
     }
 }
