8956195fcf93373d3c574d0efa2f740a4d1f3082
Elayan <blue-violin@hotmail.fr>
2021-12-10 11:09:34 +0100

    Day 6 part 2

--- AdventOfCode2021/Days/Day6.cs
+++ AdventOfCode2021/Days/Day6.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode2021.Helpers;
 
 namespace AdventOfCode2021.Days
@@ -34,6 +35,38 @@ namespace AdventOfCode2021.Days
                 finalLives.AddRange(spawns);
             }
             return finalLives;
+        }
+
+        public static void SimulateLanternFishes_Grouped()
+        {
+            var fishInitialLives = FileReader.ReadIntegerListFromFile("day6");
+            var fishFinalLives = SimulateDays_Grouped(fishInitialLives, 80, 6, 8);
+            Console.WriteLine($"[#06a][control] Lantern Fishes : fishes after 80 days = {fishFinalLives}");
+            fishFinalLives = SimulateDays_Grouped(fishInitialLives, 256, 6, 8);
+            Console.WriteLine($"[#06b] Lantern Fishes : fishes after 256 days = {fishFinalLives}");
+        }
+
+        private static long SimulateDays_Grouped(int[] fishInitialLives, int simulationDays, int resetValue, int spawnValue)
+        {
+            // compute initial groups
+            var groups = new long[spawnValue + 1];
+            for (var i = 0; i <= spawnValue; i++)
+                groups[i] = fishInitialLives.Count(f => f == i);
+
+            for (var d = 0; d < simulationDays; d++)
+            {
+                var resultGroups = new long[spawnValue + 1];
+                // all fishes with 1+ cycle day just decrease
+                for (var l = 1; l <= spawnValue; l++)
+                    resultGroups[l - 1] = groups[l];
+                // fishes with 0 cycle day produce babies and reset
+                resultGroups[spawnValue] = groups[0];
+                resultGroups[resetValue] += groups[0];
+                // save progress
+                groups = resultGroups;
+            }
+
+            return groups.Sum();
         }
     }
 }
